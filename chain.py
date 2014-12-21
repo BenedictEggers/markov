8825e6865a1aa2a96f427ef3dc4f8f2473da51a0
Ben Eggers <beggerss@cs.washington.edu>
2014-12-21 15:50:17 -0800

    chain ready to debug

--- chain.py
+++ chain.py
@@ -21,10 +21,39 @@ class MarkovNode:
         # I know, I know, it's offensive. If it becomes untenable I'll fix it
         self.children.append(token)
 
+    def has_next(self):
+        return self.children != []
+
     def next(self):
         return self.children[random.randint(len(self.children) - 1)]
 
 
 class Markov:
     def __init__(self):
-        self.start = 
+        self.start = MarkovNode("")
+        self.nodes = {}
+
+    def add_word(self, parent, next):
+        if parent not in self.nodes:
+            self.nodes[parent] = MarkovNode(parent)
+        if next not in self.nodes:
+            self.nodes[next] = MarkovNode(next)
+        self.nodes[parent].add_child(next)
+
+    def add_sentence(self, sent):
+        if "" in sent:
+            # Error handling? Nahhhhh
+            return
+
+        self.add_word("", sent[0])
+        for i in xrange(len(sent) - 1):
+            add_word(sent[i], sent[i + 1])
+
+    def generate(self):
+        sentence = ""
+        current = self.start
+        while self.nodes(current).has_next():
+            current = self.nodes[current].next()
+            sentence += current + " "
+
+        return sentence[:-1]
